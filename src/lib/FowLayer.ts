1acddb85357629759287a524b31a0b51e6547a3b
CynthiaCR <carnerocynthia@gmail.com>
2022-07-25 17:25:36 -0700

    Fow-Layer

--- src/lib/FowLayer.ts
+++ src/lib/FowLayer.ts
@@ -1,8 +1,3 @@
-
-///// OPTION 1 ///////
-////// 2 shades ///////
-
-/*
 import { Mrpas } from "mrpas";
 import type Car from "./Car";
 import type ConfigData from "./ConfigData";
@@ -61,7 +56,7 @@ export default class FowLayer{
 				if (!tile) {
 				 	continue;
 				}
-                tile.setAlpha(0);  //dark = hide all the tiles
+                tile.setAlpha(0.1);  //dark = hide all the tiles
 			}
 		}	
 	}
@@ -86,11 +81,9 @@ export default class FowLayer{
 
     let isVisible = (x:number, y:number): boolean => {
         const tile = this.roadLayer.getTileAt(x, y)
-        if (!tile || tile.alpha === 0) {
+        if (!tile) {
             return false;  
-        } else {
-            return true;  //tile.alpha > 0
-        }
+        } 
     }
 
     let setVisibility = (x:number, y:number) => {
@@ -110,12 +103,11 @@ export default class FowLayer{
     }
 }
 
-*/
 
 
 //////////////////// OPTION 2 ////////////
-/////// 3 shades ////////////////////////
-
+/////////////////////////////
+/*
 
 import { Mrpas } from "mrpas";
 import type Car from "./Car";
@@ -238,169 +230,8 @@ export default class FowLayer{
     radius, 
     isVisible,
     setVisibility);
-    }
-}
-
-
-
-//////////////////////////////////////////
-    // layerFow (map: Phaser.Tilemaps.Tilemap, tileset: string) {
-    //     this.map = map;
-    
-    //     var layer = this.map.createBlankLayer('layerFow', tileset);
-    //     //layer.setScale(1);
-    //     layer.visible;
-    //     let tileCallback = () => {
-    //         //layer.alpha = 0.5;
-    //         layer.fill(50);
-    //     }
-    //     layer.forEachTile(tileCallback)
-        
-    // }
-
-
-//////////////////////////
-/*
-import { Mrpas } from "mrpas";
-import type Car from "./Car";
-import type ConfigData from "./ConfigData";
-
-export default class FowLayer{
-	map: Phaser.Tilemaps.Tilemap;
-	roadLayer: Phaser.Tilemaps.TilemapLayer;
-	car: Car;
-	camera: Phaser.Cameras.Scene2D.CameraManager;
-    carSurrounding: Phaser.GameObjects.Graphics;
-	tileDimension: number;
-    mapHeight: number;
-    mapWidth: number;
-    tileKey: string;
-	scene: Phaser.Scene;
-    fow: Mrpas;
-    isSeen: boolean[][];
-    
-
-	constructor(mapConfigData: ConfigData) {
-        this.tileDimension = mapConfigData.tileDimension;
-        this.mapHeight = mapConfigData.mapHeight;    // height: this.mapHeight * this.tileDimension  //The height
-        this.mapWidth = mapConfigData.mapWidth;      // this.mapHeight * this.tileDimension,  //The width
-        this.tileKey = mapConfigData.tileKey;
-
-       // this.lastPosition = new Phaser.Math.Vector2({x: -1, y: -1});
-	}
-
-	mapLayer(scene: Phaser.Scene, map: Phaser.Tilemaps.Tilemap){
-        this.scene = scene;
-        this.map = map;
-       
-        const textureConfig = { 
-            width: this.mapHeight * this.tileDimension,  
-            height: this.mapHeight * this.tileDimension 
-        }
-        const tileset = this.map.addTilesetImage(this.tileKey)
-        this.roadLayer = this.map.createLayer(0, tileset, 0, 0)	
-	}
-
-    cameraFow(scene: Phaser.Scene, map: Phaser.Tilemaps.Tilemap, camera: Phaser.Cameras.Scene2D.CameraManager) {
-		this.scene = scene;
-		this.map = map;
-		this.camera = camera
-        
-		const bounds = new Phaser.Geom.Rectangle(
-			this.map.worldToTileX(this.camera.main.worldView.x),
-			this.map.worldToTileY(this.camera.main.worldView.y),
-			this.map.worldToTileX(this.camera.main.worldView.width),
-			this.map.worldToTileX(this.camera.main.worldView.height)
-		)
-	
-		for (let y = bounds.y; y < bounds.y + bounds.height; y++) {
-			for (let x = bounds.x; x < bounds.x + bounds.width; x++) {
-				if (y < 0 || y >= this.map.height || x < 0 || x >= this.map.width) {
-					continue;
-				}
-
-				const tile = this.roadLayer.getTileAt(x, y);
-				if (!tile) {
-				 	continue;
-				}
-                tile.setAlpha(0);  //dark
-			}
-		}	
-	}
-
-    createFow () {
-        let isTransparent = (x:number, y:number) => {
-            const tile = this.roadLayer.getTileAt(x, y)
-            return tile && !tile.collides;
-        }
-        this.fow = new Mrpas(this.mapHeight, this.mapWidth, isTransparent);
-    }
-    
-    calculateFow(scene: Phaser.Scene, car: Car) {
-        this.scene = scene;
-        this.car = car;
-        this.createFow();
-        this.isSeen = new Array(1000)
-
-        const px = this.map.worldToTileX(this.car.posX)
-        const py = this.map.worldToTileY(this.car.posY)
-        const radius = 2;
-
-    let isVisible = (x:number, y:number): boolean => {
-        const tile = this.roadLayer.getTileAt(x, y)
-        if (!tile) {
-            return false;  
-        }
-
-        if (tile.alpha > 0) {
-            this.isSeen[x][y] = true;
-            return true;
-        } else {
-            return false;
-        }
-        
-    }
-
-    let setVisibility = (x:number, y:number) => {
-        const tile = this.roadLayer.getTileAt(x, y)
-        if (!tile) {
-            return;
-        }
-
-        const d = Math.floor(new Phaser.Math.Vector2(x, y).distance(
-              new Phaser.Math.Vector2(px, py)));
-              
-        if (d > radius  && this.isSeen[x][y] === true) { //   && this.isSeen[x][y] == true
-            tile.setAlpha(0.5);
-            //this.layerFow (this.map, 'tiles');
-        } else {
-            tile.setAlpha(1);
-        }
-        
 
     }
-
-        this.fow.compute(
-        px,
-        py,
-        radius, 
-        isVisible,
-        setVisibility)
-    }
-}
-
-    // layerFow (map: Phaser.Tilemaps.Tilemap, tileset: string) {
-    //     this.map = map;
-    
-    //     var layer = this.map.createBlankLayer('layerFow', tileset);
-    //     //layer.setScale(1);
-    //     layer.visible;
-    //     let tileCallback = () => {
-    //         //layer.alpha = 0.5;
-    //         layer.fill(50);
-    //     }
-    //     layer.forEachTile(tileCallback)
-        
-    // }
+}  
 
 */
