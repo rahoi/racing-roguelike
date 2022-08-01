9054c8cfe8e789b73599c10c12d7bd4b7b42616d
ellen <echan13@dons.usfca.edu>
2022-08-01 16:12:08 -0700

    added clarifying comments for end scene -issue #26

--- src/lib/EndScene.ts
+++ src/lib/EndScene.ts
@@ -18,8 +18,11 @@ export default class EndScene extends Phaser.Scene {
     }
 
     create() {
+        // when viewing the entire map
         const x = this.mapConfigData.mapWidth * this.mapConfigData.tileDimension / 2;
         const y = this.mapConfigData.mapHeight * this.mapConfigData.tileDimension / 2 - 300;
+
+        // if console shows only a portion of the map
         // const x = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         // const y = this.cameras.main.worldView.y + this.cameras.main.height / 2;
 
@@ -29,10 +32,12 @@ export default class EndScene extends Phaser.Scene {
 
         this.add.text(x, y + 600, "Click to race again", {fontSize: "120px", color: "#FFFFFF"}).setOrigin(0.5)
 
+        // if we want to press any key to restart game
         // this.input.keyboard.on("keyup", () => {
         //     location.reload()
         // })
 
+        // reloads the game when pointer clicks then is let up
         this.input.on("pointerup", () =>  {
             location.reload()
         });
