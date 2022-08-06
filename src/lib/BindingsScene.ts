adbe4703086cd3501d85eddadff1713b8b6e6346
Ashley Radford <ashleyradford23@gmail.com>
2022-08-05 21:02:36 -0700

    Add flashing keys

--- src/lib/BindingsScene.ts
+++ src/lib/BindingsScene.ts
@@ -1,4 +1,4 @@
-import Phaser, { Tilemaps } from "phaser"
+import Phaser, { Tilemaps, Tweens } from "phaser"
 import type ConfigData from "./ConfigData"
 
 export default class BindingsScene extends Phaser.Scene {
@@ -17,6 +17,7 @@ export default class BindingsScene extends Phaser.Scene {
     leftKey: string;
     rightKey: string;
     pressedKey: string;
+    flashMap: Map<Phaser.GameObjects.Text, Tweens.Tween>;
 
     constructor(mapConfigData: ConfigData) {
         super("BindingsScene");
@@ -73,32 +74,69 @@ export default class BindingsScene extends Phaser.Scene {
         this.bindsArr.push(this.leftText)
         this.bindsArr.push(this.rightText)
 
+        // set up flash tweens
+        let createFlash = (key: Phaser.GameObjects.Text) => {
+            let flash = this.tweens.add({
+                targets: key,
+                duration: 400,
+                alpha: 0,
+                ease: 'Cubic.easeOut',
+                repeat: -1,
+                yoyo: true,
+                paused: true
+            })
+            return flash
+        }
+
+        // create a map of flash tweens
+        this.flashMap = new Map
+        this.bindsArr.forEach( (keyText: Phaser.GameObjects.Text) => {
+            this.flashMap.set(keyText, createFlash(keyText))
+        })
+
         // make text keys interactive
         this.bindsArr.forEach( (key) => {
             key.setInteractive()
 
             key.on('pointerover', () => {
                 this.scale.updateBounds()
-                // remove listener if previously selected key was not updated by user
-                this.bindsArr.forEach( () => {
-                    this.input.keyboard.removeListener('keydown')
-                })
                 key.setScale(1.3)
             })
 
             key.on('pointerout', () => {
                 this.scale.updateBounds()
                 key.setScale(1)
-            })
+            })  
 
             key.on('pointerdown', () => {
                 this.scale.updateBounds()
-                this.pressedKey = '_' // set empty key, waiting for user input
+                // remove listener if previously selected key was not updated by user
+                this.bindsArr.forEach( () => {
+                    this.input.keyboard.removeListener('keydown')
+                })
+
+                // go through textKeys and pause those that are not selected
+                this.flashMap.forEach( (flash, text) => {
+                    if (text === key) {
+                        flash.resume()
+                    } else {
+                        // check if flash is playing first
+                        if (flash.isPlaying()) {
+                            flash.restart()
+                            flash.pause()
+                        }
+                    }
+                })
+
+                // set empty key while we wait for user input (if any)
+                this.pressedKey = '_' 
                 key.setText(this.pressedKey)
+
+                // define pickBind function - user picks which key to bind movement to
                 let pickBind = () => {
                     this.input.keyboard.once('keydown', (event) => {
                         this.invalidText.setVisible(false)
-                        this.duplicateText.setVisible(false)
+                        this.duplicateText.setVisible(false) 
 
                         // add capture so keys will not affect browser
                         this.input.keyboard.addCapture(event.keyCode) 
@@ -116,11 +154,15 @@ export default class BindingsScene extends Phaser.Scene {
                             this.duplicateText.setVisible(true)
                             pickBind() // recusively traverse pickBind until non duplicate is selected
                         } else {
+                            // stop flashing
+                            this.flashMap.get(key).restart()
+                            this.flashMap.get(key).stop()
                             // set key in map and in text overlay
                             key.setText(this.pressedKey)    
                         }
                     })
                 }
+                // call pickBind function
                 pickBind()
             })
         })
