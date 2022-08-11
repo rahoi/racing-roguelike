aac8cbe047d04a904f8c4f2bc40db02de8eaa5ae
ellen <echan13@dons.usfca.edu>
2022-08-11 14:35:35 -0700

    modified TrackGenerationtest

--- tests/TrackGeneration.test.ts
+++ tests/TrackGeneration.test.ts
@@ -1,64 +1,54 @@
 import ConfigData from "../src/lib/ConfigData"
 import TrackGeneration from "../src/lib/TrackGeneration"
-
+import {describe, expect, test} from '@jest/globals'
+ 
+/**
+* Tests for TrackGeneration.ts
+*/
+ 
+// populate ConfigData object to use to create a new TrackGeneration object
 const tileDimension = 128;
 const tileMapHeight = 40;
 const tileMapWidth = 40;
 const backgroundColor = '#bc8044';
 const tilesetImageSheet = '/assets/spritesheet_tiles.png';
 const tileKey = 'tiles;'
-
+ 
 let mapConfigData = new ConfigData(backgroundColor, tileDimension, tileMapHeight, tileMapWidth, tilesetImageSheet, tileKey);
-
-let track:TrackGeneration = new TrackGeneration(mapConfigData);
-
-let coordinates:number[][] = [
-    [1,2],
-    [100,100],
-    [3,4],
-    [5,6],
-    [1,2],
-    [5,6],
-    [7,8],
-    [9,10],
-    [11,12],
-    [13,14],
-    [15,16],
-    [1,2],
-    [17,18],
-    [19,20],
-    // [3,4],
-    [1,2],
-    [3,4],
-    [21, 22],
-    [23, 24],
-    [25, 26],
-    [1,2]
-]
-let len:number = coordinates.length;
-console.log(coordinates);
-
-let map = new Map<string, number[]>();
-
-for (let i = 0; i < coordinates.length; i++) {
-    let coordKey:string = JSON.stringify(coordinates[i]);
-
-    if (map.get(coordKey) == null) {
-        map.set(coordKey, [i]);
-    } else {
-        map.set(coordKey, [...map.get(coordKey), i]);
-    }
-}
-
-// track.createMapArray();
-console.log("coordinate length before: ", coordinates.length);
-
-coordinates = track.removeLoops(coordinates);
-
-console.log("coordinate length after: ", coordinates.length);
-console.log("map length: ", map.size);
-
-if (len > coordinates.length) {
-    console.log(coordinates);
-    console.log("remove working");
-}
+ 
+// create new TrackGeneration object and generate a random race track
+let track = new TrackGeneration(mapConfigData);
+track.createMapArray();
+ 
+// testing track length
+test('inner race track is longer than min length and shorter than max length', () => {
+   let trackLen = track.innerTrack.length;
+   expect(trackLen).toBeGreaterThan(track.minTrackLength);
+   expect(trackLen).toBeLessThan(track.maxTrackLength);
+});
+ 
+// checking track doesn't loop back on itself (no duplicate coordinates)
+test('check track doesn\'t loop back on itself', () => {
+   let coordCount = new Map;
+ 
+   for (let i = 0; i < track.allTrackPoints.length; i++) {
+       let key = JSON.stringify(track[i]);
+ 
+       if (coordCount.get(key) == null) {
+           coordCount.set(key, 1);
+       }
+       else {
+           let count = coordCount.get(key);
+           coordCount.set(key, count++);
+       }
+   }
+ 
+   let dupCoord = false;
+   for (const [key, value] of coordCount.entries()) {
+       if (value > 1) {
+           dupCoord = true;
+       }
+   }
+ 
+   expect(dupCoord).toBe(false);
+});
