0b9039959a5e62d14f1b189d30b5554024a99d36
ellen <echan13@dons.usfca.edu>
2022-08-11 15:08:31 -0700

    update TrackGeneration tests

--- tests/TrackGeneration.test.ts
+++ tests/TrackGeneration.test.ts
@@ -1,5 +1,6 @@
 import ConfigData from "../src/lib/ConfigData"
 import TrackGeneration from "../src/lib/TrackGeneration"
+import terrainArray from "../src/lib/TerrainArray"
 import {describe, expect, test} from '@jest/globals'
  
 /**
@@ -14,36 +15,36 @@ const backgroundColor = '#bc8044';
 const tilesetImageSheet = '/assets/spritesheet_tiles.png';
 const tileKey = 'tiles;'
  
-let mapConfigData = new ConfigData(backgroundColor, tileDimension, tileMapHeight, tileMapWidth, tilesetImageSheet, tileKey);
+let mapConfigData:ConfigData = new ConfigData(backgroundColor, tileDimension, tileMapHeight, tileMapWidth, tilesetImageSheet, tileKey);
  
 // create new TrackGeneration object and generate a random race track
-let track = new TrackGeneration(mapConfigData);
+let track:TrackGeneration = new TrackGeneration(mapConfigData);
 track.createMapArray();
  
 // testing track length
 test('inner race track is longer than min length and shorter than max length', () => {
-   let trackLen = track.innerTrack.length;
+   let trackLen:number = track.innerTrack.length;
    expect(trackLen).toBeGreaterThan(track.minTrackLength);
    expect(trackLen).toBeLessThan(track.maxTrackLength);
 });
  
 // checking track doesn't loop back on itself (no duplicate coordinates)
 test('check track doesn\'t loop back on itself', () => {
-   let coordCount = new Map;
+   let coordCount:Map<string, number> = new Map;
  
    for (let i = 0; i < track.allTrackPoints.length; i++) {
-       let key = JSON.stringify(track[i]);
+       let key:string = JSON.stringify(track[i]);
  
        if (coordCount.get(key) == null) {
            coordCount.set(key, 1);
        }
        else {
-           let count = coordCount.get(key);
+           let count:number = coordCount.get(key);
            coordCount.set(key, count++);
        }
    }
  
-   let dupCoord = false;
+   let dupCoord:boolean = false;
    for (const [key, value] of coordCount.entries()) {
        if (value > 1) {
            dupCoord = true;
@@ -51,4 +52,68 @@ test('check track doesn\'t loop back on itself', () => {
    }
  
    expect(dupCoord).toBe(false);
+});
+
+// check all track coordinates are within the map's borders
+test('all track coordinates within map borders', () => {
+    let withinBorder:boolean = true;
+
+    for (let i = 0; i < track.allTrackPoints.length; i++) {
+        if (track.allTrackPoints[i][0] < 0 || track.allTrackPoints[i][0] > tileMapHeight 
+        || track.allTrackPoints[i][1] < 0 || track.allTrackPoints[i][1] > tileMapWidth) {
+            withinBorder = false;
+        }
+    }
+
+    expect(withinBorder).toBe(true);
+});
+
+// check player start is at index 0 of inner track array
+test('check player start coordinate', () => {
+    expect(track.playerStartPt).toBe(track.innerTrack[0]);
+});
+
+// check start line is at index 1 of inner track array
+test('check inner start line coordinate', () => {
+    expect(track.innerStartLinePt).toBe(track.innerTrack[1]);
+});
+
+// check inner start line tile
+test('check inner start line tile', () => {
+    let finishTile:boolean = false;
+
+    if (terrainArray.finishes.includes(track.innerStartTile)) {
+        finishTile = true;
+    }
+
+    expect(finishTile).toBe(true);
+});
+
+// check outer track start line coordinate is next to inner track start line
+test('check outer start line coordinate', () => {
+    let nextTo:boolean = false;
+
+    if (track.outerStartLinePt[0] == track.innerStartLinePt[0]) {
+        if (track.outerStartLinePt[1] == track.innerStartLinePt[1] + 1 || track.outerStartLinePt[1] == track.innerStartLinePt[1] - 1) {
+            nextTo = true;
+        }
+    }
+    else if (track.outerStartLinePt[1] == track.innerStartLinePt[1]) {
+        if (track.outerStartLinePt[0] == track.innerStartLinePt[0] + 1 || track.outerStartLinePt[0] == track.innerStartLinePt[0] - 1) {
+            nextTo = true;
+        }
+    }
+
+    expect(nextTo).toBe(true);
+});
+
+// check outer start line tile
+test('check outer start line tile', () => {
+    let finishTile:boolean = false;
+
+    if (terrainArray.finishes.includes(track.outerStartTile)) {
+        finishTile = true;
+    }
+
+    expect(finishTile).toBe(true);
 });
