ff56f512d1be119e47d5d28ed25e50822861427d
Ashley Radford <ashleyradford23@gmail.com>
2022-08-12 03:29:05 -0700

    Add Player and subclass documentation

--- src/lib/PlayerFactory.ts
+++ src/lib/PlayerFactory.ts
@@ -6,16 +6,30 @@ import Car from "./Car";
 import Bike from "./Bike";
 import Truck from "./Truck";
 
+/**
+ * PlayerFactory generates a Player factory that creates a new Player typed object from
+ * its createPlayer() method.
+ */
 export default class PlayerFactory {
     // map variables
-    map: GenerateMap;
+    map: GenerateMap
     mapConfigData: ConfigData
 
+    /**
+     * Takes as arugments the data necessary for Player object creation
+     * @param map GenerateMap object containing the generated tilemap
+     * @param mapConfigData ConfigData object containing Phaser config data
+     */
     constructor(map: GenerateMap, mapConfigData: ConfigData) {
         this.map = map;
         this.mapConfigData = mapConfigData;
     }
 
+    /**
+     * Creates a new Player typed object -- Car, Bike, or Truck
+     * @param type Player type
+     * @returns new Player typed object
+     */
     createPlayer(type: string): Player {
         switch (type) {
             case 'car': {
--- src/lib/Truck.ts
+++ src/lib/Truck.ts
@@ -1,21 +1,26 @@
-// import types
 import type ConfigData from "./ConfigData"
 import type GenerateMap from "./GenerateMap"
-
-// import classes
 import Player from "./Player"
 
+/**
+ * Stores the character specific data for Truck subclass
+ * Extends the Player class
+ */
 export default class Truck extends Player {
-
+    /**
+     * Initializes the character specific attributes Truck subclass
+     * @param map GenerateMap object containing the generated tilemap
+     * @param mapConfigData ConfigData object containing Phaser config data
+     */
     constructor(map: GenerateMap, mapConfigData: ConfigData) {
         super(map, mapConfigData);
 
-        // set player attributes 
+        /* set player attributes */
         this.wheelBase = 131            // distance between front and rear wheels
         this.steerFactor = 28           // amount that front wheel turns
-        this.enginePower = 0.0021        // forward acceleration force
-        this.brakingFactor = -0.0008   // backwards acceleration force
-        this.maxReverseSpeed = 1.2        // max reverse velocity
+        this.enginePower = 0.0021       // forward acceleration force
+        this.brakingFactor = -0.0008    // backwards acceleration force
+        this.maxReverseSpeed = 1.2      // max reverse velocity
 
         /* set environment attributes: at speed 0.4 px/ms, drag force overcomes friction force
          * see https://www.desmos.com/calculator/e4ayu3xkip */
